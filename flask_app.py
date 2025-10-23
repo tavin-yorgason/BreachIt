8ab2aa97dcf05a794b168154a42d9cbeef25b5a2
Tavin Yorgason <tazasor@gmail.com>
2025-10-22 21:34:06 -0700

    Flask integration

---
+++ flask_app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def default():
+    return redirect("/home")
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/queries')
+def queries():
+    return render_template("queries.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+if __name__ == '__main__':
+    app.run(ssl_context='adhoc')

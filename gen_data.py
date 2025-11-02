7352aaa97737e1f8ce8affbe3cef5daebcf77189
tavin-yorgason <tazasor@gmail.com>
2025-11-02 17:41:53 +0000

    Optimize data generation script

--- gen_data.py
+++ gen_data.py
@@ -11,70 +11,103 @@ TRANSACTION_COUNT = 200
 TRANSACTION_ITEM_COUNT = 300
 CART_ITEM_COUNT = 20
 
-print("Resetting database")
-try:
-    database.execute_query(
-    "DROP TABLE users, items, transactions, transaction_items, cart_items")
-except:
-    print("Error dropping tables. This probably means one or more tables\n"
-        " were already gone.")
+def main():
+    print("Resetting database")
+    try:
+        database.execute_query(
+            "DROP TABLE users, items, transactions, transaction_items, cart_items")
+    except:
+        print("Error dropping tables. This probably means one or more tables\n"
+            " were already gone.")
 
-database.execute_queries(Path("create_tables.sql").read_text())
+    database.execute_queries(Path("create_tables.sql").read_text())
 
-# Users
-print("Generating users")
-usernames = []
-for i in range(USER_COUNT):
-    usernames.append(fake.user_name())
-    email = fake.free_email()
-    birthdate = fake.date_between(start_date = '-90y')
+    # Users
+    print("Generating users")
+    usernames = []
+    columns = ["username", "email", "birthdate"]
+    values = Empty2dArray(USER_COUNT, len(columns))
+    for i in range(USER_COUNT):
+        usernames.append(fake.user_name())
+        email = fake.free_email()
+        birthdate = fake.date_between(start_date = '-90y')
 
-    database.execute_query(
-        "INSERT IGNORE INTO users (username, email, birthdate)"
-        f"VALUES ('{usernames[i]}', '{email}', '{birthdate}');")
+        values[i] = [Quote(usernames[i]), Quote(email), Quote(birthdate)]
+    ExecuteInsertQuery("users", columns, values)
 
-# Items
-print("Generating items")
-for i in range(ITEM_COUNT):
-    item_id = i
-    price = random.randint(0, 100) + 0.99
-    name = f"{fake.word()} {fake.word()}";
-    description = fake.text(max_nb_chars = 200)
-    
-    database.execute_query(
-        "INSERT IGNORE INTO items (id, price, name, description)"
-        f"VALUES ({item_id}, {price}, '{name}', '{description}');")
+    # Items
+    print("Generating items")
+    columns = ["id", "price", "name", "description"]
+    values = Empty2dArray(ITEM_COUNT, len(columns))
+    for i in range(ITEM_COUNT):
+        item_id = i
+        price = random.randint(0, 100) + 0.99
+        name = f"{fake.word()} {fake.word()}";
+        description = fake.text(max_nb_chars = 200)
 
-# Transactions
-print("Generating transactions")
-for i in range(TRANSACTION_COUNT):
-    transaction_id = i
-    username = random.choice(usernames)
-    time = fake.date_time()
-    
-    database.execute_query(
-        "INSERT IGNORE INTO transactions (id, username, time)"
-        f"VALUES ({transaction_id}, '{username}', '{str(time)}');")
+        values[i] = [str(item_id), str(price), Quote(name), Quote(description)]
+    ExecuteInsertQuery("items", columns, values)
 
-# Transaction items
-print("Generating transaction_items")
-for i in range(TRANSACTION_ITEM_COUNT):
-    transaction_id = random.randint(0, TRANSACTION_COUNT - 1)
-    item_id = random.randint(0, ITEM_COUNT - 1)
-    quantity = random.randint(1, 10)
-    price = random.randint(0, 100) + 0.99
-    
-    database.execute_query(
-        "INSERT IGNORE INTO transaction_items (transaction_id, item_id, quantity, price)"
-        f"VALUES ({transaction_id}, {item_id}, {quantity}, {price});")
+    # Transactions
+    print("Generating transactions")
+    columns = ["id", "username", "time"]
+    values = Empty2dArray(TRANSACTION_COUNT, len(columns))
+    for i in range(TRANSACTION_COUNT):
+        transaction_id = i
+        username = random.choice(usernames)
+        time = fake.date_time()
 
-# Cart items
-print("Generating cart_items")
-for i in range(TRANSACTION_ITEM_COUNT):
-    username = random.choice(usernames)
-    item_id = random.randint(0, ITEM_COUNT - 1)
-    quantity = random.randint(1, 10)
-    
-    database.execute_query(
-        "INSERT IGNORE INTO cart_items (username, item_id, quantity)"
-        f"VALUES ('{username}', {item_id}, {quantity});")
+        values[i] = [str(transaction_id), Quote(username), Quote(time)]
+    ExecuteInsertQuery("transactions", columns, values)
+
+    # Transaction items
+    print("Generating transaction_items")
+    columns = ["transaction_id", "item_id", "quantity", "price"]
+    values = Empty2dArray(TRANSACTION_ITEM_COUNT, len(columns))
+    for i in range(TRANSACTION_ITEM_COUNT):
+        transaction_id = random.randint(0, TRANSACTION_COUNT - 1)
+        item_id = random.randint(0, ITEM_COUNT - 1)
+        quantity = random.randint(1, 10)
+        price = random.randint(0, 100) + 0.99
+
+        values[i] = [str(transaction_id), str(item_id), str(quantity), str(price)]
+    ExecuteInsertQuery("transaction_items", columns, values)
+
+    # Cart items
+    print("Generating cart_items")
+    columns = ["username", "item_id", "quantity"]
+    values = Empty2dArray(CART_ITEM_COUNT, len(columns))
+    for i in range(CART_ITEM_COUNT):
+        username = random.choice(usernames)
+        item_id = random.randint(0, ITEM_COUNT - 1)
+        quantity = random.randint(1, 10)
+
+        values[i] = [Quote(username), str(item_id), str(quantity)]
+    ExecuteInsertQuery("cart_items", columns, values)
+
+def Quote(string: str) -> str:
+    return f"'{string}'"
+
+def Empty2dArray(rows, columns):
+    return [["" for i in range(columns)] for j in range(rows)]
+
+def ExecuteInsertQuery(table_name, column_names, values) -> None:
+    query = f"INSERT IGNORE INTO {table_name} ("
+
+    for name in column_names:
+        query += name + ", "
+
+    query = query[:-2] + ")\n"
+
+    query += "VALUES "
+    for value in values:
+        query += "("
+        for sub_value in value:
+            query += sub_value + ", "
+        query = query[:-2] + "),\n"
+    query = query[:-2] + ";"
+
+    database.execute_query(query)
+
+if __name__ == "__main__":
+    main()
